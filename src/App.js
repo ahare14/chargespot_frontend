92c0ce03415693c261140eb20060976749b1d231
ahare14 <alexanderhare14@gmail.com>
2019-10-01 14:39:13 -0600

    new app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,91 @@
-import React from 'react';
+import React, { Component } from 'react';
+import MapContainer from './Components/maps/mapcontainer'
+import HumanJuice from './Components/juice/humanJuice'
 import logo from './logo.svg';
 import './App.css';
+import { stat } from 'fs';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const apiEndpointCar = "http://localhost:3000/chargingstations"
+const apiEndpointPeople = "http://localhost:3000/stores"
+
+class App extends Component {
+  constructor () {
+    super ()
+    this.state = {
+      stations: [],
+      humans: [],
+      showCarJuice: false,
+      showPeopleJuice: false,
+    }
+  }
+
+  componentDidMount(){
+    this.getChargeStations(apiEndpointCar)
+    this.getStores(apiEndpointPeople)
+  }
+
+  onPeopleButtonClick = () =>
+    this.setState({
+      showPeopleJuice: true,
+      showCarJuice: false
+  });
+
+  onCarButtonClick = () =>
+    this.setState({
+      showCarJuice: true,
+      showPeopleJuice: false
+  });
+
+  getChargeStations = (apiEndpointCar) => {
+    fetch(apiEndpointCar)
+      .then(response => response.json())
+      .then(result => this.addToStateCar(result))
+      .catch(error => console.error(error))
+  }
+
+  getStores = (apiEndpointPeople) => {
+    fetch(apiEndpointPeople)
+      .then(response => response.json())
+      .then(result => this.addToStatePeople(result))
+      .catch(error => console.error(error))
+  }
+
+  addToStateCar = (result) => {
+    console.log(result)
+    this.setState(state => {
+      state.stations = result
+      return state
+    })
+  }
+  addToStatePeople = (result) => {
+    console.log(result)
+    this.setState(state => {
+      state.humans = result
+      return state
+    })
+  }
+
+  render () {
+    return (
+      <div className="mainPage">
+        <h1>Juice Me Up!</h1>
+        <button onClick={this.onPeopleButtonClick}>
+          Click Me For Human Juice
+        </button>
+        <button onClick={this.onCarButtonClick}>
+          Click Me For Car Juice
+        </button>
+        { this.state.showCarJuice === false
+          ? <HumanJuice stations={this.state.humans}></HumanJuice>
+          : <MapContainer stations={this.state.stations}/>
+
+        }
+
+
+      </div>
+    )
+  }
+
 }
 
 export default App;
---
+++ src/Components/juice/humanJuice.js
@@ -0,0 +1,79 @@
+// import React, { Component } from 'react'
+
+// export default class HumanJuice extends Component {
+//   constructor () {
+//     super ()
+//   }
+
+//   render () {
+//     return (
+//       <h1> Hello Human Juice!</h1>
+//     )
+//   }
+// }
+
+import React, { Component } from 'react';
+import { Map, GoogleApiWrapper, Marker, InfoWindow } from 'google-maps-react';
+
+class HumanJuice extends Component {
+  state = {
+    activeMarker: {},
+    selectedPlace: {},
+    showingInfoWindow: false
+  };
+
+  onMarkerClick = (props, marker) => {
+    this.setState({
+      activeMarker: marker,
+      selectedPlace: props,
+      showingInfoWindow: true
+    })
+  }
+
+  onInfoWindowClose = () =>
+    this.setState({
+      activeMarker: null,
+      showingInfoWindow: false
+    });
+
+  onMapClicked = () => {
+    if (this.state.showingInfoWindow)
+      this.setState({
+        activeMarker: null,
+        showingInfoWindow: false
+      });
+  };
+
+  plotMarker = () => (this.props.stations.map(station => (
+    <Marker onClick={this.onMarkerClick} position={{lat: station.lat, lng: station.long}} name={`Address: ${station.address}`} icon={{url:"https://img.icons8.com/color/48/000000/man-drinking-water.png"}} />)) 
+  )
+
+  render() {
+    if (!this.props.loaded) return <div>Loading...</div>;
+
+
+    return (
+      <Map
+        className="map"
+        google={this.props.google}
+        onClick={this.onMapClicked}
+        style={{ height: '80%', position: 'center', width: '99%' }}
+        zoom={13}>
+          {this.plotMarker()}
+        <InfoWindow
+          marker={this.state.activeMarker}
+          onClose={this.onInfoWindowClose}
+          visible={this.state.showingInfoWindow}>
+          <div>
+            <h4>Info: {this.state.selectedPlace.name}</h4>
+          </div>
+        </InfoWindow>
+      </Map>
+    );
+  }
+}
+
+
+export default GoogleApiWrapper({
+  apiKey: ("AIzaSyCJwIQJhwFabrSJmjbJdsXoJtRh92-TW0E")
+})(HumanJuice)
